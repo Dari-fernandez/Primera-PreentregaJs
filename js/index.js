4df2b1cd0f7a1a55f913ee99e986354408fbacdf
dariel fernandez <darifernandez223@gmail.com>
2023-12-20 13:23:09 -0300

    segunda-preentrega

--- js/index.js
+++ js/index.js
@@ -1,53 +1,55 @@
-let saludo = ("Bienvenido a Rato Express")
+alert("Ingrese la opcion del producto que desea llevar,para salir ingrese 0")
+let seleccionarProductos = Number(prompt("1-Burguer triple $3000 2-burguer doble $2000 3-burguer simple $1000 4-papas $500"))
+let seleccionarCantidad;
+let total = 0;
 
-alert(saludo)
+const cantidad = (cant, precion) => {
+    return cant * precion
+}
 
-let mensaje = "Ingrese que opcion desea\n1 - Hamburguesa simple $2000\n2 - Hamburguesa doble $3000\n3 - Hamburguesa triple $4000\n4 - Cerceza ipa $1300\n5 - Cerveza rubia $1300\n6 - Cerveza roja $1300\n7 - Coca cola $800\n8 - Papas Fritas $1500\n9 - Finalizar compra"
-let opcion
-let total = 0
 
-do {
-    opcion = Number(prompt(mensaje))
-    switch (opcion) {
+while (seleccionarProductos != 0) {
+    switch (seleccionarProductos) {
         case 1:
-            alert("Se agrega Hamburguesa simple al carrito")
-            total += 2000
-            break
+            seleccionarCantidad = Number(prompt("el producto seleccionado es Burguer triple, indique la cantidad"))
+            total += cantidad(seleccionarCantidad, 3000)
+            break;
+
         case 2:
-            alert("Se agrega Hamburguesa doble")
-            total += 3000
-            break
+            seleccionarCantidad = Number(prompt("el producto seleccionado es burguer doble, indique la cantidad"))
+            total += cantidad(seleccionarCantidad, 2000)
+            break;
+
         case 3:
-            alert("Se agrega Hamburguesa triple")
-            total += 4000
-            break
+            seleccionarCantidad = Number(prompt("el producto seleccionado es burguer simple, indique la cantidad"))
+            total += cantidad(seleccionarCantidad, 1000)
+            break;
+
         case 4:
-            alert("Se agrega Cerveza ipa")
-            total += 1300
-            break
-        case 5:
-            alert("Se agrega Cerveza rubia")
-            total += 1300
-            break
-        case 6:
-            alert("Se agrega Cerveza roja")
-            total += 1300
-            break
-        case 7:
-            alert("Se agrega Coca cola")
-            total += 800
-            break
-        case 8:
-            alert("Se agrega Papas Fritas")
-            total += 1500
-            break
-        case 9:
-            alert("Total a pagar $" + total)
-            let finalizar = prompt("Desea agregar algo mas? Si o No").toLowerCase()
-            if (finalizar === "no") {
-                alert("Gracias por su compra")
-            }
-            //default:
-            break
+            seleccionarCantidad = Number(prompt("el producto seleccionado es papas, indique la cantidad"))
+            total += cantidad(seleccionarCantidad, 500)
+            break;
+
+        default:
+            break;
+
     }
-} while (opcion != 0)
+    seleccionarProductos = Number(prompt("1-Burguer triple $3000 2-burguer doble $2000 3-burguer simple $1000 4-papas $500"))
+
+}
+
+
+alert("el total de la compra es de:" + total)
+
+const envio = () => {
+    if (total >= 10000) {
+        alert("El envio es gratuito")
+    } else {
+        total += 1000
+        alert("el costo de envio es de 1000, el total es: " + total)
+    }
+}
+
+envio()
+
+alert("Gracias por la compra! Hasta pronto ")
